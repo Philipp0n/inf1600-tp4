01e3b5592633459b8214a2da8af78ecd2539f518
jetraf <62031771+jetraf@users.noreply.github.com>
2020-04-07 17:00:58 -0400

    Update Rectangle_Perimeter.s

--- Ex_1/Rectangle_Perimeter.s
+++ Ex_1/Rectangle_Perimeter.s
@@ -8,10 +8,16 @@ _ZNK9Rectangle12PerimeterAsmEv:
         
         /* Write your solution here */
         movl 8(%ebp), %eax
+        # st[0] <- lentgth
         fld 4(%eax)
+        # st[1] <- st[0] : st[0] <- length
         fld 8(%eax)
+        # st[1] <- st[0] : st[0] <- width
         faddp
+        # st[0] <- st[0] + st[1]
         fld factor
+        st[1] <- st[0] : st[0] <- factor
         fmulp
+        # st[0] <- factor * st[0]
         leave          /* restore ebp and esp */
         ret            /* return to the caller */
