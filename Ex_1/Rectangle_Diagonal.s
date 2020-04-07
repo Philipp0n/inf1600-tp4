88f0567d1f27fd08c90623c8fa2ac7711182f1ef
jetraf <62031771+jetraf@users.noreply.github.com>
2020-04-07 17:15:47 -0400

    Update Rectangle_Diagonal.s

--- Ex_1/Rectangle_Diagonal.s
+++ Ex_1/Rectangle_Diagonal.s
@@ -7,16 +7,28 @@ _ZNK9Rectangle11DiagonalAsmEv:
         /* Write your solution here */
         movl 8(%ebp), %eax
         fld 4(%eax)
+        # st[1] <- st[0] : st[0] <- length
         fld 4(%eax)
+        # st[1] <- st[0] : st[0] <- length
         fmulp
+        # st[0] <- st[0] * st[1]
         fstp 4(%eax)
+        # st[1] <- st[0] : length <- st[0]
         fld 8(%eax)
+        # st[1] <- st[0] : st[0] <- width
         fld 8(%eax)
+        # st[1] <- st[0] : st[0] <- width
         fmulp
+        # st[0] <- st[1] * st[0]
         fstp 8(%eax)
+        # st[1] <- st[0] : width <- st[0]
         fld 4(%eax)
+        # st[1] <- st[0] : st[0] <- length
         fld 8(%eax)
+        # st[1] <- st[0] : st[0] <- width
         faddp
+        # st[0] <- st[1] + st[0]
         fsqrt
+        # st[0] <- sqrt(st[0])
         leave          /* restore ebp and esp */
         ret            /* return to the caller */
