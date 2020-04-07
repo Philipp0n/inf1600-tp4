bd9422ac615996cf3749dfc55f743d93a6148d98
jetraf <62031771+jetraf@users.noreply.github.com>
2020-04-07 17:06:31 -0400

    Update Rectangle_Area.s

--- Ex_1/Rectangle_Area.s
+++ Ex_1/Rectangle_Area.s
@@ -7,7 +7,10 @@ _ZNK9Rectangle7AreaAsmEv:
         /* Write your solution here */
         movl 8(%ebp), %eax
         fld 4(%eax)
+        # st[1] <- st[0] : st[0] <- length
         fld 8(%eax)
+        # st[1] <- st[0] : st[0] <- width
         fmulp
+        # st[0] <- st[0] * st[1]
         leave          /* restore ebp and esp */
         ret            /* return to the caller */
